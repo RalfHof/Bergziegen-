a08a86700a78e461366eb6479b7da3630ed4a7ee
RalfHof <ralf.hofman@edu.techstarter.de>
2025-02-21 16:48:23 +0100

    Deine Commit-Nachricht

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7YCsGbE8aBdZeq5KsMO3jd2YDC/tI6Kk+s6dXipcuB4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
---
+++ .next/static/chunks/app/layout-366f9dac25900dd1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{9318:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.t.bind(n,9967,23))},9967:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[310,742,441,517,358],()=>s(9318)),_N_E=e.O()}]);

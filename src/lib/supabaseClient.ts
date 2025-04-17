a235619de0ced096d670e2f215814328235b90ac
RalfHof <ralf.hofman@edu.techstarter.de>
2025-04-17 16:05:19 +0200

    fehler

--- src/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vxdxrojbgltjypxzehsg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZHhyb2piZ2x0anlweHplaHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MDg2MTEsImV4cCI6MjA2MDI4NDYxMX0.x6RaoV5RgYt5BNcO-rP_VD2_qgR0p9vkvidzoJU0lwc';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);

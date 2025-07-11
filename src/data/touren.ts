392edd0bd75fabcf5ca4f40d8bc4355685d1f73f
RalfHof <ralf.hofman@edu.techstarter.de>
2025-07-11 15:19:37 +0200

    awldblik

--- src/data/touren.ts
+++ src/data/touren.ts
@@ -4,14 +4,7 @@ export const touren = [
     name: "Alte Brauerei Weingarten",
     description: `Unsere Tour führt uns zur gemütlichen "Alten Brauerei" in Karlsruhe-Weingarten. Dort erwartet uns ein Restaurant mit charmant altem Stil, schönem Ambiente und einer freundlichen Atmosphäre. Das besondere Highlight ist das Essen vom heißen Stein, bei dem wir exotische Fleischspezialitäten und auch leckere Gemüseplatten direkt am Tisch selbst zubereiten können. Ein perfekter Ort, um den Wandertag genussvoll und in geselliger Runde ausklingen zu lassen!`,
     images: [
-      "/Tour1_1.jpg",
-      "/Tour1_2.jpg",
-      "/Tour1_3.jpg",
-      "/Tour1_4.jpg",
-      "/Tour1_5.jpg",
-      "/Tour1_6.jpg",
-      "/Tour1_7.jpg",
-      "/Tour1_8.jpg",
+      "/Tour1_1.jpg", "/Tour1_2.jpg", "/Tour1_3.jpg", "/Tour1_4.jpg", "/Tour1_5.jpg", "/Tour1_6.jpg", "/Tour1_7.jpg", "/Tour1_8.jpg"
     ],
     komootLink: "https://www.komoot.de/tour-1",
     outdooractiveLink: "https://www.outdooractive.com/de/tour-1",
@@ -24,7 +17,7 @@ export const touren = [
       "/Tour2_1.jpg", "/Tour2_2.jpg", "/Tour2_3.jpg", "/Tour2_4.jpg", "/Tour2_5.jpg",
       "/Tour2_6.jpg", "/Tour2_7.jpg", "/Tour2_8.jpg", "/Tour2_9.jpg", "/Tour2_10.jpg",
       "/Tour2_11.jpg", "/Tour2_12.jpg", "/Tour2_13.jpg", "/Tour2_14.jpg", "/Tour2_15.jpg",
-      "/Tour2_16.jpg", "/Tour2_17.jpg", "/Tour2_18.jpg",
+      "/Tour2_16.jpg", "/Tour2_17.jpg", "/Tour2_18.jpg"
     ],
     komootLink: "https://www.komoot.de/tour-2",
     outdooractiveLink: "https://www.outdooractive.com/de/tour-2",
@@ -48,14 +41,16 @@ export const touren = [
     name: "Mondbachschlucht",
     description: "Tour führt uns in die beeindruckende Monbachtalschlucht – ein echtes Naturerlebnis mit glitzernden Bächen und sattgrünen Flussauen. Der schmale Pfad, der Trittsicherheit erfordert, führt uns immer wieder über Steinquader durch das Tal. Genießt die besondere Atmosphäre dieser Schlucht, bei etwas Glück entdecken wir vielleicht sogar den seltenen Eisvogel! Zum Abschluss kehren wir in die Hausbrauerei Mönchwasen ein und lassen den Tag bei guter bürgerlicher Küche gemütlich ausklingen.",
     images: [
-      "/Tour4_1.jpg", "/Tour4_2.jpg", "/Tour4_3.jpg", "/Tour4_4.jpg", "/Tour4_5.jpg", "/Tour4_6.jpg",
-      "/Tour4_7.jpg", "/Tour4_8.jpg", "/Tour4_9.jpg", "/Tour4_10.jpg", "/Tour4_11.jpg", "/Tour4_12.jpg",
-      "/Tour4_13.jpg", "/Tour4_14.jpg", "/Tour4_15.jpg", "/Tour4_16.jpg", "/Tour4_17.jpg", "/Tour4_18.jpg",
-      "/Tour4_19.jpg", "/Tour4_20.jpg", "/Tour4_21.jpg", "/Tour4_22.jpg", "/Tour4_23.jpg", "/Tour4_24.jpg",
-      "/Tour4_25.jpg", "/Tour4_26.jpg", "/Tour4_27.jpg", "/Tour4_28.jpg", "/Tour4_29.jpg", "/Tour4_30.jpg",
-      "/Tour4_31.jpg", "/Tour4_32.jpg", "/Tour4_33.jpg", "/Tour4_34.jpg", "/Tour4_35.jpg", "/Tour4_36.jpg",
-      "/Tour4_37.jpg", "/Tour4_38.jpg", "/Tour4_39.jpg", "/Tour4_40.jpg", "/Tour4_41.jpg", "/Tour4_42.jpg",
-      "/Tour4_43.jpg", "/Tour4_44.jpg", "/Tour4_45.jpg", "/Tour4_46.jpg", "/Tour4_47.jpg", "/Tour4_48.jpg"
+      "/Tour4_1.jpg", "/Tour4_2.jpg", "/Tour4_3.jpg", "/Tour4_4.jpg", "/Tour4_5.jpg",
+      "/Tour4_6.jpg", "/Tour4_7.jpg", "/Tour4_8.jpg", "/Tour4_9.jpg", "/Tour4_10.jpg",
+      "/Tour4_11.jpg", "/Tour4_12.jpg", "/Tour4_13.jpg", "/Tour4_14.jpg", "/Tour4_15.jpg",
+      "/Tour4_16.jpg", "/Tour4_17.jpg", "/Tour4_18.jpg", "/Tour4_19.jpg", "/Tour4_20.jpg",
+      "/Tour4_21.jpg", "/Tour4_22.jpg", "/Tour4_23.jpg", "/Tour4_24.jpg", "/Tour4_25.jpg",
+      "/Tour4_26.jpg", "/Tour4_27.jpg", "/Tour4_28.jpg", "/Tour4_29.jpg", "/Tour4_30.jpg",
+      "/Tour4_31.jpg", "/Tour4_32.jpg", "/Tour4_33.jpg", "/Tour4_34.jpg", "/Tour4_35.jpg",
+      "/Tour4_36.jpg", "/Tour4_37.jpg", "/Tour4_38.jpg", "/Tour4_39.jpg", "/Tour4_40.jpg",
+      "/Tour4_41.jpg", "/Tour4_42.jpg", "/Tour4_43.jpg", "/Tour4_44.jpg", "/Tour4_45.jpg",
+      "/Tour4_46.jpg", "/Tour4_47.jpg", "/Tour4_48.jpg"
     ],
     komootLink: "https://www.komoot.de/tour-4",
     outdooractiveLink: "https://www.outdooractive.com/de/tour-4",
@@ -74,23 +69,22 @@ export const touren = [
     outdooractiveLink: "https://www.outdooractive.com/de/tour-5",
   },
   {
-  id: 6,
-  name: "Hambacher Schloss 27.05.25",
-  description: `Das Hambacher Schloss, eine Burgruine aus dem 11. Jahrhundert, spielt eine bedeutende Rolle in der deutschen Demokratiegeschichte. Es wurde am 27. Mai 1832 zum Schauplatz des Hambacher Festes, einem wichtigen Ereignis der Demokratiebewegung im 19. Jahrhundert, und gilt seither als "Wiege der deutschen Demokratie".
+    id: 6,
+    name: "Hambacher Schloss 27.05.25",
+    description: `Das Hambacher Schloss, eine Burgruine aus dem 11. Jahrhundert, spielt eine bedeutende Rolle in der deutschen Demokratiegeschichte. Es wurde am 27. Mai 1832 zum Schauplatz des Hambacher Festes, einem wichtigen Ereignis der Demokratiebewegung im 19. Jahrhundert, und gilt seither als "Wiege der deutschen Demokratie".
 
 Am 27. Mai 1832 kamen bis zu 30.000 Menschen auf dem Hambacher Schlossberg zusammen, um für ein geeintes Deutschland, politische Grundrechte und ein solidarisch verbundenes Europa einzutreten. Diese als "Hambacher Fest" bekannte Versammlung gilt als einer der bedeutendsten Meilensteine der deutschen Demokratiegeschichte.
 
 Unsere Wanderung führt uns auf etwa 9 Kilometern durch die malerische Landschaft rund um das Hambacher Schloss und überwindet dabei rund 180 Höhenmeter. Nach der Tour lädt das Weingut Ernst zur gemütlichen Einkehr ein: Dort erwartet uns gutbürgerliche Küche, eine herzliche Atmosphäre und eine schöne Sitzgelegenheit im Außenbereich – perfekt zum Wohlfühlen und Ausklingen lassen.`,
-  images: [
-    "/Tour6_1.jpg", "/Tour6_2.jpg", "/Tour6_3.jpg", "/Tour6_4.jpg", "/Tour6_5.jpg",
-    "/Tour6_6.jpg", "/Tour6_7.jpg", "/Tour6_8.jpg", "/Tour6_9.jpg", "/Tour6_10.jpg",
-    "/Tour6_11.jpg", "/Tour6_12.jpg", "/Tour6_13.jpg", "/Tour6_14.jpg", "/Tour6_15.jpg",
-    "/Tour6_16.jpg", "/Tour6_17.jpg", "/Tour6_18.jpg"
-  ],
-  komootLink: "https://www.komoot.de/tour-6",
-  outdooractiveLink: "https://www.outdooractive.com/de/tour-6",
-},
-
+    images: [
+      "/Tour6_1.jpg", "/Tour6_2.jpg", "/Tour6_3.jpg", "/Tour6_4.jpg", "/Tour6_5.jpg",
+      "/Tour6_6.jpg", "/Tour6_7.jpg", "/Tour6_8.jpg", "/Tour6_9.jpg", "/Tour6_10.jpg",
+      "/Tour6_11.jpg", "/Tour6_12.jpg", "/Tour6_13.jpg", "/Tour6_14.jpg", "/Tour6_15.jpg",
+      "/Tour6_16.jpg", "/Tour6_17.jpg", "/Tour6_18.jpg"
+    ],
+    komootLink: "https://www.komoot.de/tour-6",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-6",
+  },
   {
     id: 7,
     name: "Malsch Letzenberg",
@@ -99,16 +93,16 @@ Unsere Wanderung führt uns auf etwa 9 Kilometern durch die malerische Landsch
       "/Tour7_1.jpg", "/Tour7_2.jpg", "/Tour7_3.jpg", "/Tour7_4.jpg", "/Tour7_5.jpg",
       "/Tour7_6.jpg", "/Tour7_7.jpg", "/Tour7_8.jpg", "/Tour7_9.jpg", "/Tour7_10.jpg",
       "/Tour7_11.jpg", "/Tour7_12.jpg", "/Tour7_13.jpg", "/Tour7_14.jpg", "/Tour7_15.jpg",
-      "/Tour7_16.jpg", "/Tour7_17.jpg","/Tour7_18.jpg","/Tour7_19.jpg","/Tour7_20.jpg","/Tour7_21.jpg","/Tour7_22.jpg","/Tour7_23.jpg",
+      "/Tour7_16.jpg", "/Tour7_17.jpg", "/Tour7_18.jpg", "/Tour7_19.jpg", "/Tour7_20.jpg",
+      "/Tour7_21.jpg", "/Tour7_22.jpg", "/Tour7_23.jpg"
     ],
     komootLink: "https://www.komoot.de/tour-7",
     outdooractiveLink: "https://www.outdooractive.com/de/tour-7",
   },
-
-   {
-  id: 8,
-  name: "Mit der Draisine durch die Südpfalz",
-  description: `Muskelkraft trifft Landschaftsgenuss
+  {
+    id: 8,
+    name: "Mit der Draisine durch die Südpfalz",
+    description: `Muskelkraft trifft Landschaftsgenuss
 
 Lust auf eine Tour der etwas anderen Art? Dann ab auf die Schiene – mit der Südpfalz-Draisinenbahn! Auf der stillgelegten Bahnstrecke zwischen Landau und Germersheim kannst du mit einer Draisine, einem von Muskelkraft betriebenen Schienenfahrzeug, die wunderschöne Pfälzer Landschaft entdecken.
 
@@ -117,14 +111,35 @@ Das Ganze ist nicht nur entspanntes Dahinrollen – hier ist Teamwork, Ausdauer 
 Am Endpunkt der Strecke wartet dann die wohlverdiente Pause auf dich. Ob Picknick, Vesper oder einfach nur Füße hochlegen – hier kannst du dich stärken, durchatmen und neue Energie für die Rückfahrt tanken.
 
 Also: Draisine klarmachen, Freunde einpacken und los geht’s auf eine Tour, die garantiert in Erinnerung bleibt. Und ja, Muskelkater ist inklusive – aber sowas von verdient!`,
-  images: [
-    "/Tour8_1.jpg", "/Tour8_2.jpg", "/Tour8_3.jpg", "/Tour8_4.jpg", "/Tour8_5.jpg",
-    "/Tour8_6.jpg", "/Tour8_7.jpg", "/Tour8_8.jpg", "/Tour8_9.jpg", "/Tour8_10.jpg",
-    "/Tour8_11.jpg", "/Tour8_12.jpg", "/Tour8_13.jpg", "/Tour8_14.jpg", "/Tour8_15.jpg",
-    "/Tour8_16.jpg", "/Tour8_17.jpg", "/Tour8_18.jpg", "/Tour8_19.jpg", "/Tour8_20.jpg",
-    "/Tour8_21.jpg", "/Tour8_22.jpg"
-  ],
-  komootLink: "https://www.komoot.de/tour-8",
-  outdooractiveLink: "https://www.outdooractive.com/de/tour-8"
-}
+    images: [
+      "/Tour8_1.jpg", "/Tour8_2.jpg", "/Tour8_3.jpg", "/Tour8_4.jpg", "/Tour8_5.jpg",
+      "/Tour8_6.jpg", "/Tour8_7.jpg", "/Tour8_8.jpg", "/Tour8_9.jpg", "/Tour8_10.jpg",
+      "/Tour8_11.jpg", "/Tour8_12.jpg", "/Tour8_13.jpg", "/Tour8_14.jpg", "/Tour8_15.jpg",
+      "/Tour8_16.jpg", "/Tour8_17.jpg", "/Tour8_18.jpg", "/Tour8_19.jpg", "/Tour8_20.jpg",
+      "/Tour8_21.jpg", "/Tour8_22.jpg"
+    ],
+    komootLink: "https://www.komoot.de/tour-8",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-8"
+  },
+  {
+    id: 9,
+    name: "Neustadt an der Weinstraße",
+    description: `Unsere Tour führt uns nach Neustadt an der Weinstraße – eine der schönsten Städte entlang der Deutschen Weinstraße. Die Strecke verläuft durch dichte Waldgebiete und über zahlreiche Steigungen, was sie durchaus anspruchsvoll macht. Kondition ist gefragt – aber die Ausblicke auf die Rheinebene und die Umgebung lohnen jede Anstrengung.
+
+Ein besonderes Highlight auf dem Weg ist die historische Warte mit ihrer etwa 6 m hohen Aussichtsplattform: Der steinerne Unterbau (ca. 3 × 3 m Grundfläche) beherbergt einen kleinen Unterstand, zugänglich über eine Treppe und eine rund 2 m hohe Öffnung. Früher stand hier ein hölzerner Pavillon, heute ist nur noch ein Eisengeländer übrig. Von außen ist das Bauwerk wegen der umliegenden Bäume kaum zu erkennen – aber wer es erreicht, wird mit einem schönen Rundblick belohnt.
+
+Am Ende der Tour lädt der Pfalzblick zur verdienten Einkehr ein – es sei denn, er hat gerade Betriebsferien. Dann bleibt nur eines: Rucksackvesper auspacken, Füße hochlegen und den Tag in der Natur genießen. Eine fordernde, aber lohnenswerte Tour mit Geschichte, Höhenmetern und Aussicht!
+!`,
+    images: [
+      "/Tour9_1.jpg", "/Tour9_2.jpg", "/Tour9_3.jpg", "/Tour9_4.jpg", "/Tour9_5.jpg",
+      "/Tour9_6.jpg", "/Tour9_7.jpg", "/Tour9_8.jpg", "/Tour9_9.jpg", "/Tour9_10.jpg",
+      "/Tour9_11.jpg", "/Tour9_12.jpg", "/Tour9_13.jpg", "/Tour9_14.jpg", "/Tour9_15.jpg",
+      "/Tour9_16.jpg", "/Tour9_17.jpg", "/Tour9_18.jpg", "/Tour9_19.jpg", "/Tour9_20.jpg",
+      "/Tour9_21.jpg", "/Tour9_22.jpg", "/Tour9_23.jpg", "/Tour9_24.jpg", "/Tour9_25.jpg",
+      "/Tour9_26.jpg", "/Tour9_27.jpg", "/Tour9_28.jpg", "/Tour9_29.jpg", "/Tour9_30.jpg",
+      "/Tour9_31.jpg", "/Tour9_32.jpg", "/Tour9_33.jpg", "/Tour9_34.jpg", "/Tour9_35.jpg"
+    ],
+    komootLink: "https://www.komoot.de/tour-9",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-9"
+  }
 ];

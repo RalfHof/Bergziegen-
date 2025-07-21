f9a22c9e5b2217d289e854737832b5a9f2b3ca6a
RalfHof <ralf.hofman@edu.techstarter.de>
2025-07-21 14:32:51 +0200

    1111

---
+++ src/data/touren.ts
@@ -0,0 +1,98 @@
+export const touren = [
+  {
+    id: 1,
+    name: "Alte Brauerei Weingarten",
+    description: `"Alte Brauerei Weingarten",
+    description: Unsere Tour führt uns zur gemütlichen "Alten Brauerei" in Karlsruhe-Weingarten. Dort erwartet uns ein Restaurant mit charmant altem Stil, schönem Ambiente und einer freundlichen Atmosphäre. Das besondere Highlight ist das Essen vom heißen Stein, bei dem wir exotische Fleischspezialitäten und auch leckere Gemüseplatten direkt am Tisch selbst zubereiten können. Ein perfekter Ort, um den Wandertag genussvoll und in geselliger Runde ausklingen zu lassen!,...`,
+    images: [
+      "/Tour1_1.jpg", "/Tour1_2.jpg", "/Tour1_3.jpg", "/Tour1_4.jpg", "/Tour1_5.jpg", "/Tour1_6.jpg", "/Tour1_7.jpg", "/Tour1_8.jpg"
+    ],
+    komootLink: "https://www.komoot.de/tour-1",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-1",
+  },
+  {
+    id: 2,
+    name: "Besen Hexe Kreutzbergsee",
+    description: "Unsere Tour führt uns zur Besenhex Tiefenbach am Kreutzbergsee. Dort erwartet uns ein besonders attraktives Angebot: ein reichhaltiges Buffet für nur 24 Euro pro Person, bei dem alle Getränke bereits inklusive sind! Freut euch auf eine riesige Auswahl an Speisen in dieser schönen Lokalität. Ein wirklich entspannter und leckerer Abschluss für unsere Wanderung",
+    images: [
+      "/Tour2_1.jpg", "/Tour2_2.jpg", "/Tour2_3.jpg", "/Tour2_4.jpg", "/Tour2_5.jpg", "/Tour2_6.jpg", "/Tour2_7.jpg", "/Tour2_8.jpg", "/Tour2_9.jpg", "/Tour2_10.jpg",
+      "/Tour2_11.jpg", "/Tour2_12.jpg", "/Tour2_13.jpg", "/Tour2_14.jpg", "/Tour2_15.jpg", "/Tour2_16.jpg", "/Tour2_17.jpg", "/Tour2_18.jpg"
+    ],
+    komootLink: "https://www.komoot.de/tour-2",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-2",
+  },
+  {
+    id: 3,
+    name: "Forsthaus Silbertal",
+    description: "Unsere Tour entführt uns in das Herz des Biosphärenreservates Pfälzerwald – eine sehr schöne Wanderung durch den Wald mit vielen Bächen und Sehenswürdigkeiten. Ziel ist das idyllisch und ruhig gelegene Forsthaus Silbertal auf rund 410m Höhe. Dort erwartet uns eine Einkehr mit Pfälzer Gerichten, Wurst aus eigener Herstellung sowie weiteren regionalen und vegetarischen Spezialitäten. Bei gutem Wetter können wir die Natur auf dem Freisitz genießen – der ideale Ausklang für diese schöne Waldtour!",
+    images: Array.from({ length: 25 }, (_, i) => `/Tour3_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-3",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-3",
+  },
+  {
+    id: 4,
+    name: "Mondbachschlucht",
+    description: "Tour führt uns in die beeindruckende Monbachtalschlucht – ein echtes Naturerlebnis mit glitzernden Bächen und sattgrünen Flussauen. Der schmale Pfad, der Trittsicherheit erfordert, führt uns immer wieder über Steinquader durch das Tal. Genießt die besondere Atmosphäre dieser Schlucht, bei etwas Glück entdecken wir vielleicht sogar den seltenen Eisvogel! Zum Abschluss kehren wir in die Hausbrauerei Mönchwasen ein und lassen den Tag bei guter bürgerlicher Küche gemütlich ausklingen",
+    images: Array.from({ length: 49 }, (_, i) => `/Tour4_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-4",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-4",
+  },
+  {
+    id: 5,
+    name: "Holz Weißbrot",
+    description: "Beschreibung der Tour 5",
+    images: Array.from({ length: 17 }, (_, i) => `/Tour5_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-5",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-5",
+  },
+  {
+    id: 6,
+    name: "Hambacher Schloss 27.05.25",
+    description: `Hambacher Schloss 27.05.25",
+  description: Das Hambacher Schloss, eine Burgruine aus dem 11. Jahrhundert, spielt eine bedeutende Rolle in der deutschen Demokratiegeschichte. Es wurde am 27. Mai 1832 zum Schauplatz des Hambacher Festes, einem wichtigen Ereignis der Demokratiebewegung im 19. Jahrhundert, und gilt seither als "Wiege der deutschen Demokratie...
+  Unsere Wanderung führt uns auf etwa 9 Kilometern durch die malerische Landschaft rund um das Hambacher Schloss und überwindet dabei rund 180 Höhenmeter. Nach der Tour lädt das Weingut Ernst zur gemütlichen Einkehr ein: Dort erwartet uns gutbürgerliche Küche, eine herzliche Atmosphäre und eine schöne Sitzgelegenheit im Außenbereich – perfekt zum Wohlfühlen und Ausklingen lassen.`,
+    images: Array.from({ length: 18 }, (_, i) => `/Tour6_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-6",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-6",
+  },
+  {
+    id: 7,
+    name: "Malsch Letzenberg",
+    description: "Beschreibung der Tour 7",
+    images: Array.from({ length: 23 }, (_, i) => `/Tour7_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-7",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-7",
+  },
+  {
+    id: 8,
+    name: "Mit der Draisine durch die Südpfalz",
+    description: `Muskelkraft trifft Landschaftsgenuss
+
+Lust auf eine Tour der etwas anderen Art? Dann ab auf die Schiene – mit der Südpfalz-Draisinenbahn! Auf der stillgelegten Bahnstrecke zwischen Landau und Germersheim kannst du mit einer Draisine, einem von Muskelkraft betriebenen Schienenfahrzeug, die wunderschöne Pfälzer Landschaft entdecken.
+
+Das Ganze ist nicht nur entspanntes Dahinrollen – hier ist Teamwork, Ausdauer und ein gutes Sitzfleisch gefragt. Die Strecke hat es in sich, aber genau das macht den Spaß aus! Unterwegs gibt's jede Menge zu sehen, zu lachen und zu strampeln – je nachdem, wie sportlich du’s angehst.
+
+Am Endpunkt der Strecke wartet dann die wohlverdiente Pause auf dich. Ob Picknick, Vesper oder einfach nur Füße hochlegen – hier kannst du dich stärken, durchatmen und neue Energie für die Rückfahrt tanken.
+
+Also: Draisine klarmachen, Freunde einpacken und los geht’s auf eine Tour, die garantiert in Erinnerung bleibt. Und ja, Muskelkater ist inklusive – aber sowas von verdient!,...`,
+    images: Array.from({ length: 22 }, (_, i) => `/Tour8_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-8",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-8",
+  },
+  {
+    id: 9,
+    name: "Gimmeldingen (Neustadt an der Weinstraße)",
+    description: "Unsere Tour führt uns nach Neustadt an der Weinstraße – eine der schönsten Städte entlang der Deutschen Weinstraße. Die Strecke verläuft durch dichte Waldgebiete und über zahlreiche Steigungen, was sie durchaus anspruchsvoll macht. Kondition ist gefragt – aber die Ausblicke auf die Rheinebene und die Umgebung lohnen jede Anstrengung. ",
+    images: Array.from({ length: 35 }, (_, i) => `/Tour9_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-9",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-9",
+  },
+  {
+    id: 10,
+    name: "Salzbergwerk Bad Friedrichshall",
+    description: "Beschreibung der Tour 10 - Das Salzbergwerk ist ein faszinierender Ort mit über 180 Metern Tiefe...",
+    images: Array.from({ length: 47 }, (_, i) => `/Tour10_${i + 1}.jpg`),
+    komootLink: "https://www.komoot.de/tour-10",
+    outdooractiveLink: "https://www.outdooractive.com/de/tour-10",
+  }
+];

74771842ae13f4d46cedb22f23ed94b031100c40
RalfHof <ralf.hofman@edu.techstarter.de>
2025-09-27 11:59:06 +0200

    ohne footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,21 @@ import '../styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { SessionProvider } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  // Footer nur auf der Startseite sichtbar
+  const showFooter = pathname === '/';
+
   return (
     <html lang="de">
       <body className="site-wrapper">
         <SessionProvider>
           <Header />
           <main>{children}</main>
-          <Footer />
+          {showFooter && <Footer />}
         </SessionProvider>
       </body>
     </html>

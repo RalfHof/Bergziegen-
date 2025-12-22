7fd0ceaafb65df30f1fa79e326802a47d46d7240
RalfHof <ralf.hofman@edu.techstarter.de>
2025-12-22 18:28:33 +0100

    222222

---
+++ src/app/community/page.tsx
@@ -0,0 +1,11 @@
+// app/community/page.tsx
+'use client';
+
+export default function CommunityPage() {
+  return (
+    <div>
+      <h1>Bergziegen Community</h1>
+      <p>Hier entsteht unsere Community-Seite.</p>
+    </div>
+  );
+}
